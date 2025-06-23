618b66c1cd08397eb963d573179cc69b1e3612e4
Marco A. <161896294+MarcoALR@users.noreply.github.com>
2025-06-23 14:07:22 -0300

    🎈

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.jsx"; 
+import Home from "./Home.jsx"; 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/qr" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
